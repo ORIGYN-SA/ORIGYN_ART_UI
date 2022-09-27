2bbb9ce640d461248fc3e182668b967be8f2075c
Frank Andreas <andreass.frankk@gmail.com>
2022-09-27 12:25:51 +0300

    added the component

--- src/components/interface/Dropdown/Dropdown.stories.tsx
+++ src/components/interface/Dropdown/Dropdown.stories.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
 import Dropdown from "./Dropdown";
-import ArrowRight from "../../icons/ArrowRight";
-import Flex from "../../layout/Flex";
 
 export default {
   title: "Components/Interface/Dropdown/All stories",
   component: Dropdown,
 } as Meta;
 
-const Template: Story = (args) => <>
-   <Dropdown placeHolder="Select..." />
-</>;
+const Template: Story = (args) => (
+  <div style={{ height: "200px" }}>
+    <Dropdown placeHolder="Select..." {...args} />
+  </div>
+);
 
 export const primary = Template.bind({});
-primary.args = {};
-
-
-
+primary.args = {
+  options: [
+    { value: "green", label: "Green" },
+    { value: "red", label: "Red" },
+    { value: "blue", label: "Blue" },
+    { value: "pink", label: "Pink" },
+  ],
+};
